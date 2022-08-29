270e27fbed68eb8d520be7ca518c70fbbb61df56
Abraham Dulla <abrish.d143t@gmail.com>
2022-08-29 08:00:40 +0300

    simple updates

--- Javascript/signin.js
+++ Javascript/signin.js
@@ -1,46 +1,44 @@
-var database = [ 
-    {
-        username: "Abraham",
-        password: "abrish"
-    },
-    {
-        username: "Surafel",
-        password: "surpog"
-    },
-    {
-        username: "Mulualem",
-        password: "mule"
-    },
-    {
-        username: "Abdulsemed",
-        password: "abd"
-    }
-
+var database = [
+  {
+    username: "Abraham",
+    password: "abrish",
+  },
+  {
+    username: "Surafel",
+    password: "surpog",
+  },
+  {
+    username: "Mulualem",
+    password: "mule",
+  },
+  {
+    username: "Abdulsemed",
+    password: "abd",
+  },
 ];
 
+function signIn(username, password) {
+  var username = document.getElementById("username-input").value;
+  var password = document.getElementById("password-input").value;
 
-function signIn(username, password){
-
-    var username = document.getElementById("username-input").value;
-    var password = document.getElementById("password-input").value;
-    
-    for(i=0; i<database.length; i++){
-        if(username === database[i].username && password === database[i].password){
-            window.location.href = "dashboard.html";
-            document.getElementById("app-bar-info").innerHTML = username+" You are logged in";
-            return
-        }
+  for (i = 0; i < database.length; i++) {
+    if (
+      username === database[i].username &&
+      password === database[i].password
+    ) {
+      window.location.href = "dashboard.html";
+      document.getElementById("app-bar-info").innerHTML =
+        username + " You are logged in";
+      return;
     }
-
-        document.getElementById("info").innerHTML = " Incorrect username or password";
+  }
+  document.getElementById("info").innerHTML = " Incorrect username or password";
 }
 
-
-
-function signOut(){
-    if (confirm("Are you sure you want to logout ?") == true) {
-        window.location.href = "signIn.html";
-    } else {
-        window.location.href = "#";
-    }
+function signOut() {
+  if (confirm("Are you sure you want to logout ?") == true) {
+    window.location.href = "signIn.html";
+  } else {
+    window.location.href = "#";
+  }
 }
